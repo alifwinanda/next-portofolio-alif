9d141640852365bbe9a39dd113163c37b6e01793
Alif Winanda <alif.winanda@ralali.com>
2024-03-25 16:00:44 +0700

    feat : working hidden navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
-"use client"
-import styled from "@emotion/styled"
-import { Center, Container } from "@chakra-ui/react"
-import WelcomeSection from "@/components/WelcomeSection"
-import NavBar from "@/components/NavBar"
+"use client";
+import styled from "@emotion/styled";
+import { Center, Container } from "@chakra-ui/react";
+import WelcomeSection from "@/components/WelcomeSection";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <Container maxW="5xl" margin="30px auto">
+      <Container maxW="5xl" margin="80px auto auto auto">
+        <WelcomeSection />
+        <WelcomeSection />
+        <WelcomeSection />
+        <WelcomeSection />
         <WelcomeSection />
       </Container>
     </>
-  )
+  );
 }
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,45 +1,73 @@
-import React, { useState } from "react"
-import {
-  Button,
-  ButtonGroup,
-  Container,
-  Flex,
-  Heading,
-  Spacer,
-} from "@chakra-ui/react"
-import styled from "@emotion/styled"
-import theme from "@/common/theme"
+import React, { useEffect, useState } from "react";
+import { Button, Container, Flex, Heading, Spacer } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+import theme from "@/common/theme";
 
 const NavbarWrapper = styled.div`
-  width: 100%;
-  background-color: ${theme.colors.aliceBlue.DEFAULT};
-  padding: 8px 0px;
-`
+  .active {
+    display: flex;
+    align-items: center;
+    height: 4rem;
+    width: 100%;
+    position: fixed;
+    top: 0px;
+    transition: 0.3s linear;
+    background-color: ${theme.colors.aliceBlue.DEFAULT};
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    z-index: 1;
+    box-shadow: 0 2px 5px -1px rgba(0, 0, 0, 0.08);
+  }
+  .hidden {
+    top: -80px;
+    transition: 0.3s linear;
+  }
+`;
 
 function NavBar() {
-  const [isDarkMode, setIsDarkmode] = useState<boolean>(false)
-
+  const [isDarkMode, setIsDarkmode] = useState<boolean>(false);
   const handleClickDarkMode = () => {
-    setIsDarkmode(!isDarkMode)
-  }
+    setIsDarkmode(!isDarkMode);
+  };
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+
+    if (currentScrollPos < prevScrollPos) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
 
   return (
     <NavbarWrapper>
-      <Container maxW="4xl">
-        <Flex alignContent={"space-between"}>
-          <Heading>Alif&apos;s Portofolio</Heading>
-          <Spacer />
-          <Button
-            onClick={handleClickDarkMode}
-            backgroundColor={isDarkMode ? "#130430" : "#c9b7eb"}
-            color={isDarkMode ? "#c9b7eb" : "#130430"}
-          >
-            {isDarkMode ? "Dark Mode" : "Light Mode"}
-          </Button>
-        </Flex>
-      </Container>
+      <div className={visible ? "active hidden" : "active"}>
+        <Container maxW="4xl">
+          <Flex alignContent={"space-between"}>
+            <Heading>Alif&apos;s Portofolio</Heading>
+            <Spacer />
+            <Button
+              onClick={handleClickDarkMode}
+              backgroundColor={isDarkMode ? "#130430" : "#c9b7eb"}
+              color={isDarkMode ? "#c9b7eb" : "#130430"}
+            >
+              {isDarkMode ? "Dark Mode" : "Light Mode"}
+            </Button>
+          </Flex>
+        </Container>
+      </div>
     </NavbarWrapper>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
